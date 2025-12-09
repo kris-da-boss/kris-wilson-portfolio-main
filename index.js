a874ec8a98c2292f20043fc964f83985ef53fb45
Kris Wilosn <kriswilson00000@gmail.com>
2025-12-10 00:30:30 +0100

    changed some styles

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let showMenu=false;
+let showForm=false;
 
-const menu=document.querySelector('.menuButton')
-const menuDiv=document.querySelector('.menu')
+const menu=document.querySelector('.menuButton');
+const menuDiv=document.querySelector('.menu');
 const closeMenu=document.querySelector('.closeMenu');
 const closeMenu2=document.querySelector('.navigations')
+
 
 menu.addEventListener('click', function(){
     showMenu=true;
@@ -15,12 +17,58 @@ closeMenu.addEventListener('click' ,()=> {
 })
  function displayMenu(){
    if(showMenu){
-    menuDiv.classList.add('showMenu')
+    menuDiv.classList.add('showMenu');
+    document.body.style.overflow='hidden'
      }else{
         menuDiv.classList.remove('showMenu')
+        document.body.style.overflow='auto'
      }
  }
+
  closeMenu2.addEventListener('click', function(){
     showMenu=false
    displayMenu()
  })
+
+//  Form animation
+const galaxy=document.querySelector('.galaxy');
+function shadowValues(color){
+let shadowValue='';
+
+for(let i=0; i<20; i++){
+   const x=Math.floor(Math.random() * window.innerWidth)
+   const y=Math.floor(Math.random() * window.innerHeight)
+   console.log(x)
+   shadowValue+=`${x}px ${y}px ${color},`
+}
+
+return shadowValue.slice(0,-1)
+}
+ const stars=shadowValues('#fff')
+   galaxy.style.boxShadow=`${stars}`
+   galaxy.style.setProperty('--box-shadow', `${stars}` )
+
+  //  Form Show and Hide
+  const formButton=document.querySelector('.formButton');
+  const form=document.querySelector('.form-mobile');
+const formOverlay=document.querySelector('.formOverlay');
+  formButton.addEventListener('click',()=>{
+      showForm=true;
+      toggleForm()
+  })
+  formOverlay.addEventListener('click',()=>{
+    showForm=false;
+    toggleForm()
+    formOverlay.style.display='none'
+  })
+
+  function toggleForm(){
+    if(showForm){
+      form.classList.add('showForm')
+      formOverlay.style.display='block';
+       document.body.style.overflow='hidden'
+    }else{
+       form.classList.remove('showForm')
+        document.body.style.overflow='auto'
+    }
+  }
